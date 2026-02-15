59d8544988e16f13a88a71faae5ec0fb3fabeec3
Ninter6 <mc525740@outlook.com>
2026-02-16 01:46:39 +0800

    simplification

---
+++ README.md
@@ -0,0 +1,9 @@
+# donut
+A toy program, rendering a spinning donut.
+![](https://p.sda1.dev/30/13044eea97260c007006563765e3dc4b/CleanShot%202026-02-16%20at%2001.26.53_副本_converted.gif)
+
+# build & run
+```
+g++ donut.cc -o donut
+./donut
+```
--- donut.cc
+++ donut.cc
@@ -5,64 +5,38 @@
 
 using namespace mathpls;
 
-constexpr int WIDTH = 80;
-constexpr int HEIGHT = 23;
-
-constexpr float R0 = 1, R1 = .5;
-constexpr float D0 = 12, D1 = 72;
+constexpr int WIDTH = 80, HEIGHT = 23;
+constexpr float R0 = 1, R1 = .5, D0 = 72, D1 = 72;
 
 constexpr std::string_view tb = ".-:+=*#";
+
+const mat4 p = perspective(1.f, (float)WIDTH/HEIGHT/2, .1f,  10.f);
+const vec3 ld = normalize(vec3{5, 4, 2});
 
 char img[HEIGHT][WIDTH+1]{};
 float dep[HEIGHT][WIDTH]{};
 
-mat4 vp;
-vec3 cam{0, 0, -4};
-
-const auto ld = normalize(vec3{5, 4, 2});
-auto lum(const vec3& u) {
-    auto N = normalize(u - vec3(vec2(u)).normalized()*R0);
-    return tb[(int)round((tb.size()-1)*pow(dot(N, ld)*.5+.5, 3))];
-}
-
-void pt(const vec3& u) {
-    auto q = vp * vec4(u, 1.f);
-    q = q / q.w * .5f + .5f;
-    int cy = round(q.y * (HEIGHT-1)), cx = round(q.x * (WIDTH-1));
-    if (dep[cy][cx] < q.z) {
-        dep[cy][cx] = q.z;
-        img[cy][cx] = lum(u);
-    }
-}
-
-void gen() {
-    for (float h = 0; h <= .785f; h += .785f/D0) {
-        const auto shr = sin(h)*R1, chr = cos(h)*R1;
-        for (float t = 0; t < pi()*2; t += pi()/D1*2) {
-            const auto ct = cos(t), st = sin(t);
-            pt({ct*(R0-shr), st*(R0-shr), chr});
-            pt({ct*(R0+shr), st*(R0+shr), chr});
-            pt({ct*(R0-shr), st*(R0-shr),-chr});
-            pt({ct*(R0+shr), st*(R0+shr),-chr});
-            pt({ct*(R0-chr), st*(R0-chr), shr});
-            pt({ct*(R0+chr), st*(R0+chr), shr});
-            pt({ct*(R0-chr), st*(R0-chr),-shr});
-            pt({ct*(R0+chr), st*(R0+chr),-shr});
-        }
-    }
-}
-
 int main() {
-    for (float t=0;;t+=.006f) {
-        vp = perspective(1.f, (float)WIDTH/HEIGHT/2, .1f,  10.f) *
-            lookAt(cam, {}, {0,-1, 0});
-        gen();
+    for (float t=0;;t+=.005f) {
+        vec3 cam = quat({cos(t-.3),sin(t)*3.14, 0}, xyz) * vec3(2);
+        auto vp = p * lookAt(cam, {}, {0,-1, 0});
+
+        for (float i = 0; i < pi()*2; i += pi()/D0*2)
+            for (float j = 0; j < pi()*2; j += pi()/D1*2) {
+                vec3 u = {cos(j)*(R0+sin(i)*R1), sin(j)*(R0+sin(i)*R1), cos(i)*R1};
+                auto q = vp * vec4(u, 1.f);
+                q = q / q.w * .5f + .5f;
+                int cy = round(q.y * (HEIGHT-1)), cx = round(q.x * (WIDTH-1));
+                if (dep[cy][cx] < q.z) {
+                    dep[cy][cx] = q.z;
+                    auto N = normalize(u - vec3(vec2(u)).normalized()*R0);
+                    img[cy][cx] = tb[(int)round((tb.size()-1)*pow(dot(N, ld)*.5+.5, 3))];
+                }
+            }
 
         std::cout << "\E[2J\E[H";
         for (auto& s : img)
             puts(s), std::memset(s, ' ',  WIDTH);
         std::memset(dep, 0, WIDTH*HEIGHT*sizeof(float));
-
-        cam = quat({cos(t-.3),sin(t)*3.14, 0}, xyz) * vec3(2);
     }
 }
